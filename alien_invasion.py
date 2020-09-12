3d3337be98cd72d689aa0309eb668801e1fbb8d9
foxyol <69712602+foxyol@users.noreply.github.com>
2020-09-12 12:26:42 +0300

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,14 +20,25 @@ class AlienInvasion:
     def run_game(self):
         '''Запуск основного цикла игры'''
         while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
             # Отслеживает события клавиатуры и мыши
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.type == pygame.KEYUP:
+                        self.ship.moving_right = False
 
+    def _update_screen(self):
             # При каждом проходе цикла перерисовывается цвет экрана
             self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()        
+            self.ship.blitme()     
                 
             # Отображение последнего прорисованного экрана
             pygame.display.flip()
